02bf1ee160bf9313e5191b13ba08ea199af387ee
lucas cordeiro da Silva <lukas_civil@hotmail.com>
2021-06-01 01:42:32 -0300

    feat: add github article renders

--- src/features/article-as-alert.ts
+++ src/features/article-as-alert.ts
@@ -5,12 +5,37 @@ import $ from "jquery";
  * Renders
  */
 
-const renderArticleQuestionsAlert = (): void => {
+const renderQuestionsArticleAlert = (): void => {
   $('.chat-line__body:contains("?")').closest(".chat-line__bubble").attr({
     style: "background: #f59a6b;",
   });
 };
 
+const renderGitHubMergedArticleAlert = (): void => {
+  $('.chat-line__body:contains("merged pull request")')
+    .closest(".chat-line__bubble")
+    .attr({
+      style: "background: #bca4e6;",
+    });
+};
+
+const renderGitHubReadyForReviewArticleAlert = (): void => {
+  $('.chat-line__body:contains("/pull/")').closest(".chat-line__bubble").attr({
+    style: "background: #7ed182",
+  });
+};
+
+const renderGitHubOpennedPullRequestArticleAlert = (): void => {
+  $('.chat-line__body:contains("opened pull request")')
+    .closest(".chat-line__bubble")
+    .attr({
+      style: "background: #b0b0b0",
+    });
+};
+
 export const renderArticleAsAlert = (): void => {
-  renderArticleQuestionsAlert();
+  renderQuestionsArticleAlert();
+  renderGitHubMergedArticleAlert();
+  renderGitHubReadyForReviewArticleAlert();
+  renderGitHubOpennedPullRequestArticleAlert();
 };
