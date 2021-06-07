f092df4b07f759be858d2b811dc5efea2cf28100
lukascivil <lukas_civil@hotmail.com>
2021-06-07 02:04:46 -0300

    fix: config fields were pending when app is updated with new fields

--- src/helpers/db.ts
+++ src/helpers/db.ts
@@ -4,17 +4,30 @@ import {
   ParsedConfig,
 } from "../models/basecamp-essentials-config";
 
+// Helpers
+import { compareKeys } from "./form";
+
 const configEntityName = "config";
-const defaultParsedConfig: ParsedConfig = { chatSummary: "false" };
+const defaultParsedConfig: ParsedConfig = {
+  chatSummary: "false",
+  coloredChatBorder: "false",
+};
 
 export const getConfig = (): Promise<ParsedConfig> => {
   return new Promise((resolve) => {
     chrome.storage.sync.get(configEntityName, (storage) => {
       const configStorage = storage as ConfigStorage;
 
-      if (!configStorage?.config) {
-        setConfig(defaultParsedConfig).then(() => {
-          resolve(defaultParsedConfig);
+      if (
+        !configStorage?.config ||
+        !compareKeys(configStorage?.config, defaultParsedConfig)
+      ) {
+        const newConfig: ParsedConfig = {
+          ...defaultParsedConfig,
+          ...configStorage.config,
+        };
+        setConfig(newConfig).then(() => {
+          resolve(newConfig);
         });
 
         return;
